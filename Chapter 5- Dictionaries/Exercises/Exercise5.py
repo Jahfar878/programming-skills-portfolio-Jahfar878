8cd31c2abdc52a560ad182588b804d7e306cffdc
Jahfar878 <144889894+Jahfar878@users.noreply.github.com>
2023-11-30 10:34:57 +0400

    Create Exercise5.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise5.py
@@ -0,0 +1,27 @@
+pets = []
+
+pet = {
+    'animal type': 'dog',
+    'name': 'sheezuu',
+    'owner': 'jahfar',
+}
+pets.append(pet)
+
+pet = {
+    'animal type': 'Cat',
+    'name': 'andrew',
+    'owner': 'jahafar',
+}
+pets.append(pet)
+
+pet = {
+    'animal type': 'cat',
+    'name': 'tommy',
+    'owner': 'jahfar',
+}
+pets.append(pet)
+
+for pet in pets:
+    print(f"\n what I know about {pet['name']}:")
+    for key, value in pet.items():
+        print(f"\t{key}: {value}")
